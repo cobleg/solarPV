f7bb94b0b662924cde78f95d13c4fa27a7d11a4d
Grant Coble-Neal <grantcn96@gmail.com>
2022-05-27 13:37:09 +0800

    testing the clearskies library

--- R/GHI Clear Skies function.R
+++ R/GHI Clear Skies function.R
@@ -3,7 +3,7 @@
 # Objective: create a function to calculate clear-skies global horizontal irradiance (GHI_clear)
 # Author: Grant Coble-Neal
 
-GHI_clear <- function( SolarZenithAngle, AirMass, Elevation, Interval )
+GHI_clear <- function( SolarZenithAngle, AirMass, Elevation )
 {
   # This is the Ineichen model of clear skies Global Horizontal Irradiance (GHI)
   # Elevation is defined in metres above sea level
@@ -22,14 +22,21 @@ GHI_clear <- function( SolarZenithAngle, AirMass, Elevation, Interval )
   ghi = c_g1 * I_0 * cos( SolarZenithAngle * pi / 180) * T_L / T_L * ghi_1
   
   GHI = c_g1 * I_0 * cos( SolarZenithAngle * pi / 180) * exp( -c_g2 * AirMass * (f_h1 + f_h2 * (T_L -1))) * exp(0.01 * AirMass^(1.8))
-  
-  return( ghi )
+  GHI[GHI<0] <- 0
+  return( GHI )
 }
 
 dec <- SolarDeclination(DayAngle(seq(1:1461), 365.25))
-SHA <- SolarZenithAngle(Latitude = -31.95, SolarDeclination = dec, SolarHourAngle = SHA)
+SHA <- SolarZenithAngle(Latitude = -31.95, SolarDeclination = dec, SolarHourAngle = SHA, Interval = 1)
 AM <- AirMass( SolarZenithAngle( Latitude = -31.95, SolarDeclination = dec, SolarHourAngle = SHA ) )
 
-SolarIrradiance <- GHI_clear( SolarZenithAngle = SHA, AirMass = AM, Elevation = 22, Interval = 60) 
+SolarIrradiance <- data.frame ( GHI = GHI_clear( SolarZenithAngle = SHA, AirMass = AM, Elevation = 22) )
 
-plot( SolarIrradiance[1:48] )
+params <- list(a = 0.50572, b = 6.07995, c = 1.6364, TL = 3) # defaults
+day <- list(DayOfYear = 1, Year = 2022, Interval = 1)
+
+GHI_cs <- clearskies::clear_sky("Ineichen", x = day, longitude = 115.86, latitude = -31.95, elevation = 22, parameters = params, tz = 8)
+GHI_cs$predicted[GHI_cs$predicted<1] <- 0
+
+plot( SolarIrradiance$GHI[1:48], type = 'l', xlab = 'hour of the day', ylab = 'Wh/m^2', main = 'Global Horizontal Irradiance')
+plot(GHI_cs$predicted, xlab = ' minutes of the day',ylab = 'Wh/m^2', main = 'Clear Skies Ineichen model')
--- R/Solar Zenith Angle function.R
+++ R/Solar Zenith Angle function.R
@@ -3,17 +3,19 @@
 # Objective: create a function to calculate the solar zenith angle
 # Author: Grant Coble-Neal
 
-SolarZenithAngle <- function( Latitude, SolarDeclination, SolarHourAngle )
+SolarZenithAngle <- function( Latitude, SolarDeclination, SolarHourAngle, Interval )
 {
   # is the angle between the local zenith and the line joining the observer and the sun. 
   # In astronomy, the zenith is the point in the sky directly above the observer. 
   
+  interval = (Interval - 60)/60
+  
   return( acos( sin( Latitude * pi / 180 ) * sin( SolarDeclination * pi / 180 ) + 
-            cos( Latitude * pi / 180) * cos ( SolarDeclination * pi /180) * cos( SolarHourAngle * pi / 180) ) * 180 / pi )
+            cos( Latitude * pi / 180) * cos ( SolarDeclination * pi /180) * cos( SolarHourAngle * interval * pi / 180) ) * 180 / pi )
   # units of measurement: degrees
 }
 
 dec <- SolarDeclination(DayAngle(seq(1:1461), 365.25))
 SHA <- SolarHourAngle(t_sol(TC(L_st = 120, L_loc = 115.86, (EoT(DayAngle(seq(1:1461), 365.25)))), DaylightSavingsTime = FALSE))
 
-SolarZenithAngle(Latitude = -31.95, SolarDeclination = dec, SolarHourAngle = SHA)
+SolarZenithAngle(Latitude = -31.95, SolarDeclination = dec, SolarHourAngle = SHA, Interval = 1)
